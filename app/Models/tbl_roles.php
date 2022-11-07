9ca52223e0967cef230cbc18cb3e2656dba04354
Camilo Diaz <camilo1003diaz@gmail.com>
2022-11-07 12:49:27 -0500

    Correccion articulo

--- app/Models/tbl_roles.php
+++ app/Models/tbl_roles.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class tbl_roles extends Model
 {
+    protected $primaryKey = 'cod_rol';
     use HasFactory;
 }
--- app/Models/tbl_usuarios.php
+++ app/Models/tbl_usuarios.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class tbl_usuarios extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'id_user';
 }
